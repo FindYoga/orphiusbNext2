a540641fa2b46924c6e194bf774abc44a473c4fd
Alex Grant <alex@findyoga.com.au>
2024-06-02 18:43:20 +1000

    new build

---
+++ .next/static/73kcVRT62Vj-wLA3-q6Xb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5fe3b46e612acf1c.css","static/chunks/pages/index-2a46a562810e89cc.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
